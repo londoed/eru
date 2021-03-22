4c7888beefb470d5ef032c5395e1b1cf19c1b1a3
Eric Londo <londoed@comcast.net>
2021-03-22 03:46:49 -0400

    Cleaned up some uneeded calls

--- eru.c
+++ eru.c
@@ -63,8 +63,12 @@ eru_draw_rows(void)
 {
 	int i;
 
-	for (i = 0; i < eru.screen_rows; i++)
-		write(STDOUT_FILENO, "~\r\n", 3);
+	for (i = 0; i < eru.screen_rows; i++) {
+		write(STDOUT_FILENO, "~", 1);
+
+		if (i < eru.screen_rows - 1)
+			write(STDOUT_FILENO, "\r\n", 2);
+	}
 }
 
 void
@@ -96,14 +100,47 @@ get_window_size(int *rows, int *cols)
 {
 	struct winsize ws;
 
-	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == - 1 || ws.ws_col == 0) {
-		return -1;
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+			return -1;
+
+		return get_cursor_position(rows, cols);
 	} else {
 		*cols = ws.ws_col;
 		*rows = ws.ws_row;
 
 		return 0;
 	}
+}
+
+int
+get_cursor_position(int *rows, int *cols)
+{
+	char buf[32];
+	unsigned int i = 0;
+
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+		return -1;
+
+	while (i < sizeof(buf) - 1) {
+		if (read(STDIN_FILENO, &buf[i], 1) != 1)
+			break;
+
+		if (buf[i] == 'R')
+			break;
+
+		i++;
+	}
+
+	buf[i] = '\0';
+	
+	if (buf[0] != '\x1b' || buf[1] != '[')
+		return -1;
+
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+		return -1;
+
+	return 0;
 }
 
 void
--- eru.h
+++ eru.h
@@ -16,6 +16,19 @@
 
 #define CTRL_KEY(k) ((k) & 0x1F)
 
+void eru_error(const char *);
+void disable_raw_mode(void);
+void enable_raw_mode(void);
+void eru_draw_rows(void);
+void eru_clear_screen(void);
+void eru_read_key(void);
+
+int get_window_size(int *, int *);
+int get_cursor_position(int *, int *);
+void eru_process_keypress(void);
+
+void eru_init(void);
+
 struct Editor {
 	struct termios orig;
 	int screen_rows, screen_cols;
